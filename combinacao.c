31706294d877939ce9f09ff15ca29b7b753b70d7
Regimar <60851078+regimarnegreiros@users.noreply.github.com>
2024-04-28 09:53:15 -0300

    Update combinacao.c

--- combinacao.c
+++ combinacao.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
 int combinacao(int n, int p) {
-	if (p > n) return 0;
+    if (p > n) return 0;
     if (p == 0 || p == n) return 1;
     if (p == 1 || p == n-1) return n;
-	return combinacao(n - 1, p - 1) + combinacao(n - 1, p);
+    return combinacao(n - 1, p - 1) + combinacao(n - 1, p);
 }
 
 int main() {
     int qlinhas = 10;
-    int qcolinicial = 1;
+    int qcolunasinicial = 1;
 
     for (int i = 0; i < qlinhas; i++) {
-        for(int j = 0; j < qcolinicial; j++) {
+        for(int j = 0; j < qcolunasinicial; j++) {
             printf("%3d ", combinacao(i, j));
         }
-        qcolinicial++;
+        qcolunasinicial++;
         printf("\n");
     }
     return 0;
